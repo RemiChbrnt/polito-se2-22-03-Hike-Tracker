2b198171b5391325b37588064c422ea10b06ba2b
LorenzoSciara <s303452@studenti.polito.it>
2022-11-10 19:17:30 +0100

    get hikes list front end API

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -6,7 +6,7 @@
 const URL = 'http://localhost:3001/api';
 
 
-
+/* user/logIn/logOut/signUp API */
 
 async function login(email, password) {
     const response = await fetch(URL + '/login', {
@@ -30,8 +30,6 @@ async function login(email, password) {
     }
 }
 
-
-
 async function signup() {
     const response = await fetch(URL + '/signup', { credentials: 'include' });
     const user = await response.json();
@@ -43,5 +41,27 @@ async function signup() {
     }
 }
 
-const API = { login, signup };
+
+/* hikes API */
+
+async function getAllHikes() {
+    // call: GET /api/hikes
+    const response = await fetch(URL + '/hikes', { credentials: 'include' });
+    const hikesJson = await response.json();
+    if (response.ok) {
+      return riddlesJson.map((r) => ({ title:r.title, 
+        length:r.length, 
+        expTime:r.expTime, 
+        ascent:r.ascent, 
+        difficulty:r.difficulty, 
+        startPt:r.startPt, 
+        endPt:r.endPt, 
+        description:r.description 
+    }))
+    } else {
+      throw hikesJson;  // mi aspetto che sia un oggetto json fornito dal server che contiene l'errore
+    }
+  }
+
+const API = { login, signup, getAllHikes };
 export default API;
