cdf7695a057103f64f04af33880ab6d917428a24
LorenzoSciara <s303452@studenti.polito.it>
2022-11-10 18:59:56 +0100

    Login, SignUp style redone

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -39,33 +39,59 @@ function LoginForm(props) {
     }
 
 
+    const handleBack = (event) => {
+        event.preventDefault();
+        setEmail('');
+        setPassword('');
+        setError(false);
+        navigate('/');
+    }
 
     return (
-        <div style={{ width: "50vw" }}>
+        <Container>
+            <ul></ul>
+            <Row className="justify-content-md-center">
+                <Col md="auto">
+                    <h2 className="text-center">Log-In</h2>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Email" required={true}
+                                onChange={ev => { setEmail(ev.target.value) }}
+                            />
+                        </Form.Group>
 
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="text" placeholder="Email" required={true}
-                        onChange={ev => { setEmail(ev.target.value) }}
-                    />
-                </Form.Group>
-
-
-                <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required={true}
-                        onChange={ev => { setPassword(ev.target.value) }}
-                    />
-                </Form.Group>
-
-                <Button type="submit">Login</Button>
-
-                {error && <span>Error: email or password not correct</span>}
-
-            </Form>
-
-        </div>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" required={true}
+                                onChange={ev => { setPassword(ev.target.value) }}
+                            />
+                        </Form.Group>
+                        <ul></ul>
+                        <Row>
+                            <Col md={4} xs={4}>
+                                <Button onClick={handleBack} variant='danger' size='lg'>Back</Button>{" "}
+                            </Col>
+                            <Col md={2} xs={2}></Col>
+                            <Col md={6} xs={6}>
+                                <Button type="submit" variant='success' size='lg'>Log-In</Button>
+                            </Col>
+                        </Row>
+                        {error && <span>Error: email or password not correct</span>}
+                    </Form>
+                </Col>
+            </Row>
+            <ul></ul>
+            <Row className="justify-content-md-center">
+                <Col md="auto">
+                <h3 className="text-center">Are you new here? Create your account</h3>
+                <ul></ul>
+                <div className="d-grid gap-2">
+                    <Button onClick={() => { navigate('/signup') }} variant='outline-success' size='lg'>Sign-Up</Button>
+                </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
