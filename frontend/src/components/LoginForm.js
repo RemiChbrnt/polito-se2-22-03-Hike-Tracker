3ef62449e57a21255a5450cffbdedad34d751471
Enrico Jansen <s304160@studenti.polito.it>
2022-11-10 08:45:22 +0100

    Added login and signup forms

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,70 @@
+import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const handlerLogin = async (username, password) => {
+    try {
+        // let user = await API.login(username, password);        
+        return true;
+
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+};
+
+
+function LoginForm() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    let navigate = useNavigate();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        let result = await handlerLogin(email, password);
+
+        if (result !== false)
+            navigate(`/home`);
+        else
+            setError(true);
+    }
+
+
+
+    return (
+        <div style={{ width: "50vw" }}>
+
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="text" placeholder="Email" required={true}
+                        onChange={ev => { setEmail(ev.target.value) }}
+                    />
+                </Form.Group>
+
+
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required={true}
+                        onChange={ev => { setPassword(ev.target.value) }}
+                    />
+                </Form.Group>
+
+                <Button type="submit">Login</Button>
+
+                {error && <span>Error: email or password not correct</span>}
+
+            </Form>
+
+        </div>
+    );
+}
+
+
+export { LoginForm };
