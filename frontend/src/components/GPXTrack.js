1747b2860f5c49f2e25e8a1a6e8e5e28f3a953ef
unknown <remi.chabrant@gmail.com>
2022-11-21 17:50:15 +0100

    Displaying the track for hikes that possess one

--- frontend/src/components/GPXTrack.js
+++ frontend/src/components/GPXTrack.js
@@ -1,34 +1,10 @@
-import {useState} from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet';
+import {useMap} from 'react-leaflet';
 import * as L from 'leaflet';
-import toGeoJson from '@mapbox/togeojson'
-import { DOMParser } from 'xmldom'
+
 
 const GPXTrack = (props) => {    
     const map = useMap();
-    if (props.file != undefined){
-        if (props.file != null){
-            console.log(props.file.file);
-            let reader = new FileReader();
-            reader.readAsText(props.file.file);
-            reader.onloadend =() => {
-                var gpx = new DOMParser().parseFromString(reader.result);
-                var converted = toGeoJson.gpx(gpx);
-                
-                console.log("test");
-                console.log(converted);
-                // console.log(JSON.stringify(converted));
-                var startPointCoordinates = converted.features[0].geometry.coordinates[0];
-                var endPointCoordinates = converted.features[0].geometry.coordinates[converted.features[0].geometry.coordinates.length-1];
-
-                console.log(startPointCoordinates);
-                console.log(endPointCoordinates);
-                new L.geoJSON(converted).addTo(map);
-            
-            }
-        }
-    }
-    return (null);
+    new L.geoJSON(JSON.parse(props.geoJsonFile)).addTo(map);
 }
 
 export default GPXTrack;
--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,14 +1,13 @@
 import {useState} from "react";
-import {Container} from 'react-bootstrap';
 import { MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet';
 import * as L from 'leaflet';
 import GPXTrack from "./GPXTrack";
+import { HikeGrid } from "./hikeList";
 
 const Map = (props) => {
     const startPt = JSON.parse(props.startPt);
     const endPt = JSON.parse(props.endPt);
     const [windowSize, setWindowSize] = useState(getWindowSize());
-    const [file, setFile]=useState(null);
     
     function getWindowSize() {
         const {innerWidth, innerHeight} = window;
@@ -23,40 +22,27 @@ const Map = (props) => {
         iconUrl: require('../images/finish-marker.png'),
         iconSize: [104, 158]
     });
-
-    const handleFile = (ev) => {
-        let uploadedFile = ev.target.files[0];
-        if(uploadedFile.name.slice(-4) != ".gpx"){
-            console.log("aaa");
-        }else{
-            console.log(uploadedFile);
-            setFile({file:uploadedFile});
-        }
-    }
-
    
     return (
-        <Container>
-            <b> Upload a GPX Track for the hike </b>
-            <input type="file" name="file" onChange={ev => handleFile(ev)}/>
-            <MapContainer center={[startPt.latitude, startPt.longitude]} zoom={12} scrollWheelZoom={false} style={{width: windowSize.innerWidth/2.5, height: windowSize.innerHeight/1.5}} >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                    <Marker position={[startPt.latitude, startPt.longitude]} icon={startIcon}>
-                        <Popup>
-                            Starting point<br />
-                        </Popup>
-                    </Marker>
-                    <Marker position={[endPt.latitude, endPt.longitude]} icon={finishIcon}>
-                        <Popup>
-                            Finish point<br />
-                        </Popup>
-                    </Marker>
-                    <GPXTrack file={file}/>
-            </MapContainer>
-        </Container>
+        <MapContainer center={[startPt.latitude, startPt.longitude]} zoom={12} scrollWheelZoom={false} style={{width: windowSize.innerWidth/2.5, height: windowSize.innerHeight/1.5}} >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+                <Marker position={[startPt.latitude, startPt.longitude]} icon={startIcon}>
+                    <Popup>
+                        Starting point<br />
+                        {(startPt.address !== null)&&startPt.address}
+                    </Popup>
+                </Marker>
+                <Marker position={[endPt.latitude, endPt.longitude]} icon={finishIcon}>
+                    <Popup>
+                        Arrival point<br />
+                        {(endPt.address !== null)&&endPt.address}
+                    </Popup>
+                </Marker>
+                {(props.file !== undefined) && <GPXTrack geoJsonFile={props.file}/>}
+        </MapContainer>
     );
 }
 
