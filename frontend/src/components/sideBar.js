829658b5f2862b64cbfcf913992dcd16bb27b1cb
LorenzoSciara <s303452@studenti.polito.it>
2022-11-22 15:29:00 +0100

    Sidebar update

--- frontend/src/components/sideBar.js
+++ frontend/src/components/sideBar.js
@@ -2,9 +2,14 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container, Button, Navbar, Offcanvas, Nav } from "react-bootstrap";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 function SideBar(props) {
+
+    const location = useLocation();
+
+    const [over, setOver] = useState("");
     const navigate = useNavigate();
 
     return (
@@ -25,9 +30,38 @@ function SideBar(props) {
                         props.user.role === "guide" ?
                             <Offcanvas.Body>
                                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                                    <Nav.Link onClick={() => { navigate('/add-hike-description') }} style={{ color: "white" }} active><h5>Add hike description</h5></Nav.Link>
-                                    <Nav.Link onClick={() => { navigate('/add-parking-lot') }} style={{ color: "white" }}><h5>Add parking lot</h5></Nav.Link>
-                                    <Nav.Link onClick={() => { navigate('/add-hike-description') }} style={{ color: "white" }}><h5>Link hut to hike</h5></Nav.Link>
+                                    {location.pathname === '/' ?
+                                        <Nav.Link onClick={() => { navigate('/') }} onMouseOver={() => { setOver('hike-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Hike list</h5></Nav.Link>
+                                        :
+                                        over === 'hike-list' ?
+                                            <Nav.Link onClick={() => { navigate('/') }} onMouseOver={() => { setOver('hike-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Hike list</h5></Nav.Link>
+                                            :
+                                            <Nav.Link onClick={() => { navigate('/') }} onMouseOver={() => { setOver('hike-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "white", backgroundColor: "#00706c" }}><h5>{' '}Hike list</h5></Nav.Link>
+                                    }
+                                    {location.pathname === '/add-hike-description' ?
+                                        <Nav.Link onClick={() => { navigate('/add-hike-description') }} onMouseOver={() => { setOver('add-hike-description') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Add hike description</h5></Nav.Link>
+                                        :
+                                        over === 'add-hike-description' ?
+                                            <Nav.Link onClick={() => { navigate('/add-hike-description') }} onMouseOver={() => { setOver('add-hike-description') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Add hike description</h5></Nav.Link>
+                                            :
+                                            <Nav.Link onClick={() => { navigate('/add-hike-description') }} onMouseOver={() => { setOver('add-hike-description') }} onMouseLeave={() => { setOver('') }} style={{ color: "white", backgroundColor: "#00706c" }}><h5>{' '}Add hike description</h5></Nav.Link>
+                                    }
+                                    {location.pathname === '/add-parking-lot' ?
+                                        <Nav.Link onClick={() => { navigate('/add-parking-lot') }} onMouseOver={() => { setOver('add-parking-lot') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Add parking lot</h5></Nav.Link>
+                                        :
+                                        over === 'add-parking-lot' ?
+                                            <Nav.Link onClick={() => { navigate('/add-parking-lot') }} onMouseOver={() => { setOver('add-parking-lot') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Add parking lot</h5></Nav.Link>
+                                            :
+                                            <Nav.Link onClick={() => { navigate('/add-parking-lot') }} onMouseOver={() => { setOver('add-parking-lot') }} onMouseLeave={() => { setOver('') }} style={{ color: "white", backgroundColor: "#00706c" }}><h5>{' '}Add parking lot</h5></Nav.Link>
+                                    }
+                                    {location.pathname === '/link-hut-to-hike' ?
+                                        <Nav.Link onClick={() => { navigate('/link-hut-to-hike') }} onMouseOver={() => { setOver('link-hut-to-hike') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Link hut to hike</h5></Nav.Link>
+                                        :
+                                        over === 'link-hut-to-hike' ?
+                                            <Nav.Link onClick={() => { navigate('/link-hut-to-hike') }} onMouseOver={() => { setOver('link-hut-to-hike') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Link hut to hike</h5></Nav.Link>
+                                            :
+                                            <Nav.Link onClick={() => { navigate('/link-hut-to-hike') }} onMouseOver={() => { setOver('link-hut-to-hike') }} onMouseLeave={() => { setOver('') }} style={{ color: "white", backgroundColor: "#00706c" }}><h5>{' '}Link hut to hike</h5></Nav.Link>
+                                    }
                                     <ul></ul>
                                     <Button onClick={() => { props.setUser(undefined) }} variant='danger'><i className="bi bi-box-arrow-in-right"></i> Log-Out{" "}</Button>
                                 </Nav>
@@ -70,7 +104,22 @@ function SideBar(props) {
                                         : props.user.role === "hiker" ?
                                             <Offcanvas.Body> {/*HIKER*/}
                                                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                                                    <Nav.Link onClick={() => { navigate('/hut-list') }} style={{ color: "white" }}><h5>Hut list</h5></Nav.Link>
+                                                    {location.pathname === '/' ?
+                                                        <Nav.Link onClick={() => { navigate('/') }} onMouseOver={() => { setOver('hike-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Hike list</h5></Nav.Link>
+                                                        :
+                                                        over === 'hike-list' ?
+                                                            <Nav.Link onClick={() => { navigate('/') }} onMouseOver={() => { setOver('hike-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Hike list</h5></Nav.Link>
+                                                            :
+                                                            <Nav.Link onClick={() => { navigate('/') }} onMouseOver={() => { setOver('hike-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "white", backgroundColor: "#00706c" }}><h5>{' '}Hike list</h5></Nav.Link>
+                                                    }
+                                                    {location.pathname === '/hut-list' ?
+                                                        <Nav.Link onClick={() => { navigate('/hut-list') }} onMouseOver={() => { setOver('hut-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Hut list</h5></Nav.Link>
+                                                        :
+                                                        over === 'hut-list' ?
+                                                            <Nav.Link onClick={() => { navigate('/hut-list') }} onMouseOver={() => { setOver('hut-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "black", backgroundColor: "white" }}><h5>{' '}Hut list</h5></Nav.Link>
+                                                            :
+                                                            <Nav.Link onClick={() => { navigate('/hut-list') }} onMouseOver={() => { setOver('hut-list') }} onMouseLeave={() => { setOver('') }} style={{ color: "white", backgroundColor: "#00706c" }}><h5>{' '}Hut list</h5></Nav.Link>
+                                                    }
                                                     <ul></ul>
                                                     <Button onClick={() => { props.setUser(undefined) }} variant='danger'><i className="bi bi-box-arrow-in-right"></i> Log-Out{" "}</Button>
                                                 </Nav>
