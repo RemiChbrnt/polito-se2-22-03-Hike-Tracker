cb2bcf1344d947e2ab50e192f361276199654c40
Enrico Jansen <s304160@studenti.polito.it>
2022-11-23 14:13:07 +0100

    Draft hut testing

--- backend/unit_test/__tests__/hut.test.js
+++ backend/unit_test/__tests__/hut.test.js
@@ -1,41 +1,49 @@
-// const mockHutDAO = require('./mockDAOs/mockHutDAO');
-// const HutService = require('../api/services/hutService');
-// const service = new HutService(mockHutDAO);
+const HutDAO = require('../../api/DAOs/hutDAO');
+const HutService = require('../../api/services/hutService');
+const service = new HutService(HutDAO);
 
-// const SECONDS = 1000;
-// jest.setTimeout(20 * SECONDS);
+const SECONDS = 1000;
+jest.setTimeout(20 * SECONDS);
 
-// const hutsLocation = [{
-//     id: 1,
-//     name: "Rifugio La Riposa",
-//     type: "hut",
-//     latitude: 45.1788097585636,
-//     longitude: 7.08152295397762,
-//     country: "Italy",
-//     province: "TO",
-//     town: "Mompantero",
-//     address: "Frazione La Riposa",
-//     altitude: "2185.0"
-// }];
+const hutsLocation = [{
+    id: 1,
+    name: "Rifugio La Riposa",
+    type: "hut",
+    latitude: 45.1788097585636,
+    longitude: 7.08152295397762,
+    country: "Italy",
+    province: "TO",
+    town: "Mompantero",
+    address: "Frazione La Riposa",
+    altitude: "2185.0"
+}];
 
-// const huts = [{
-//     locationId,
-//     numberOfBeds,
-//     food,
-//     description,
-//     openingTime,
-//     closingTime,
-//     cost
-// }];
-
-
-// describe("Hut tests", () => {
-//     beforeEach(() => {
-//         mockHutDAO.getHuts.mockReset();
-//         mockHutDAO.getHuts.mockResolvedValue([{
-
-//         }])
+const huts = [{
+    locationId,
+    numberOfBeds,
+    food,
+    description,
+    openingTime,
+    closingTime,
+    cost
+}];
 
 
-//     })
-// });
+describe("Hut tests", () => {
+    beforeEach(async () => {
+        await HutDAO.clearDatabase();
+
+
+    })
+
+
+    test('Create new hut', async () => {
+
+    });
+
+
+
+
+
+
+});
