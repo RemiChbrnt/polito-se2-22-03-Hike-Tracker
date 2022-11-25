c3a3aef40aa9edb7e61c062d9a4d0ff6e587d7fc
Enrico Jansen <s304160@studenti.polito.it>
2022-11-25 15:18:26 +0100

    Update for location tests

--- backend/unit_test/__tests__/location.test.js
+++ backend/unit_test/__tests__/location.test.js
@@ -75,12 +75,12 @@ describe("Hut tests", () => {
 
     test('Create valid hut 1', async () => {
         const result = await LocationDao.addLocation(validHuts[0]);
-        return expect(result).toEqual(201);
+        return expect(result).toEqual(validHuts[0]);
     });
 
     test('Create valid hut 2', async () => {
         const result = await LocationDao.addLocation(validHuts[1]);
-        return expect(result).toEqual(201);
+        return expect(result).toEqual(validHuts[1]);
     });
 
 
@@ -130,181 +130,76 @@ describe("Hut tests", () => {
 });
 
 
+
+
+
 describe('Parking lot tests', () => {
-    beforeEach(async () => {
+    beforeAll(async () => {
         await resetLocations();
     });
 
-    test('The operation of creating a new parking should complete without errors', async () => {
-        const newParking = {
-            "name": "Park La Maria",
-            "type": "parkinglot",
-            "latitude": 45,
-            "longitude": 17,
-            "altitude": 1300,
-            "country": "Italy",
-            "province": "TO",
-            "town": "fakeTown",
-            "address": null,
-            "description": "bla bla bla",
-            "lotsNumber": 14
-        };
-        const res = await LocationDao.addLocation(newParking);
-        expect(res).toBe(201);
+    const validParkingLot = [{
+        "name": "Park La Maria",
+        "type": "parkinglot",
+        "latitude": 45,
+        "longitude": 17,
+        "altitude": 1300,
+        "country": "Italy",
+        "province": "TO",
+        "town": "fakeTown",
+        "address": null,
+        "description": "bla bla bla",
+        "lotsNumber": 14
+    },
+    {
+        "name": "Park x",
+        "type": "parkinglot",
+        "latitude": 43,
+        "longitude": 18,
+        "altitude": 130,
+        "country": "Italy",
+        "province": "TO",
+        "town": "town fake",
+        "address": null,
+        "description": "goo description",
+        "lotsNumber": 10
+    }];
+
+    const invalidParkingLot = {
+        "name": "Park La Maria",
+        "type": "parkinglot",
+        "latitude": 45,
+        "longitude": null,
+        "altitude": 1300,
+        "country": "Italy",
+        "province": "TO",
+        "town": "fakeTown",
+        "address": null,
+        "description": "bla bla bla",
+        "lotsNumber": 14
+    };
+
+
+
+    test('Create valid parking lot 1', async () => {
+        const res = await LocationDao.addLocation(validParkingLot[0]);
+        return expect(res).toEqual(expect.objectContaining(validParkingLot[0]));
     });
 
-    test('The operation of creating a new parking should fail in case of missing coordinates', async () => {
-        const newParking = {
-            "name": "Park La Maria",
-            "type": "parkinglot",
-            "latitude": 45,
-            "longitude": null,
-            "altitude": 1300,
-            "country": "Italy",
-            "province": "TO",
-            "town": "fakeTown",
-            "address": null,
-            "description": "bla bla bla",
-            "lotsNumber": 14
-        };
+    test('Create valid parking lot 2', async () => {
+        const res = await LocationDao.addLocation(validParkingLot[1]);
+        return expect(res).toEqual(expect.objectContaining(validParkingLot[1]));
+    });
+
+    test('Create invalid parking lot (missing coordinates)', async () => {
         try {
-            const result = await LocationDao.addLocation(newParking);
+            const result = await LocationDao.addLocation(invalidParkingLot);
         } catch (err) {
             console.log("err " + err);
             return expect(err).toEqual(400);
         }
     });
+
+
 })
 
-
-
-// const validParkingLots = [{
-//     name: "parking lot 1",
-//     latitude: 45,
-//     longitude: 45,
-//     country: "Italy",
-//     province: "TO",
-//     town: "A",
-//     address: "address xyz",
-//     altitude: 500,
-//     lotsNumber: 20,
-//     description: "not good"
-// },
-// {
-//     name: "parking lot 2",
-//     latitude: 70,
-//     longitude: 54.438953,
-//     country: "Italy",
-//     province: "TO",
-//     town: "B",
-//     address: "address abc",
-//     altitude: 1000,
-//     lotsNumber: 50,
-//     description: "good"
-// }];
-
-
-// const invalidParkingLots = [{
-//     name: "parking lot 1",
-//     latitude: "latitude x",
-//     longitude: 4.35472,
-//     country: "Italy",
-//     province: "TO",
-//     town: "A",
-//     address: "address xyz",
-//     altitude: "abcdef",
-//     lotsNumber: 20,
-//     description: "not good"
-// },
-// {
-//     name: "parking lot 2",
-//     latitude: 70,
-//     longitude: 54.438953,
-//     country: 111,
-//     province: "TO",
-//     town: "B",
-//     address: "address abc",
-//     altitude: -1000,
-//     lotsNumber: "abc",
-//     description: "good"
-// }]
-
-
-
-// // describe("Parking lot tests", () => {
-//     beforeAll(async () => {
-//         await LocationDao.clearDatabase();
-//     })
-
-//     test('Create valid parking lot 1', async () => {
-//         const result = await LocationDao.addParkingLot(validParkingLots[0]);
-//         return expect(result).toEqual(201);
-//     });
-
-//     test('Create valid parking lot 2', async () => {
-//         const result = await LocationDao.addParkingLot(validParkingLots[1]);
-//         return expect(result).toEqual(201);
-//     });
-
-//     test('Create invalid parking lot 1', async () => {
-//         try {
-//             const result = await LocationDao.addParkingLot(invalidParkingLots[0]);
-//         } catch (err) {
-//             return expect(err).toEqual(400);
-//         }
-//     });
-
-//     test('Create invalid parking lot 2', async () => {
-//         try {
-//             const result = await LocationDao.addParkingLot(invalidParkingLots[1]);
-//         } catch (err) {
-//             return expect(err).toEqual(400);
-//         }
-
-//     });
-
-
-//     test('Valid get all valid huts', async () => {
-
-//         const result = await LocationDao.getParkingLots({});
-
-//         let retrievedValidParkingLots = validParkingLots;
-//         retrievedValidHuts[0].type = "parkinglot";
-//         retrievedValidHuts[1].type = "parkinglot";
-
-//         return expect(result).toEqual(expect.arrayContaining([expect.objectContaining({
-//             name: expect.any(String),
-//             type: expect.stringMatching("hut"),
-//             latitude: expect.any(Number),
-//             longitude: expect.any(Number),
-//             country: expect.any(String),
-//             province: expect.any(String),
-//             town: expect.any(String),
-//             address: expect.any(String),
-//             altitude: expect.any(Number),
-//             numberOfBeds: expect.any(Number),
-//             food: expect.any(String),
-//             description: expect.any(String)
-//         })]));
-//     });
-
-//     test('Valid get valid parking lot 1', async () => {
-//         const result = await LocationDao.getHuts(validHuts[0]);
-//         return expect(result[0]).toEqual(expect.objectContaining(validHuts[0]));
-//     });
-
-//     test('Valid get valid parking lot 2', async () => {
-//         const result = await LocationDao.getHuts(validHuts[1]);
-//         return expect(result[0]).toEqual(expect.objectContaining(validHuts[1]));
-//     });
-
-//     test('Invalid get parking lot 2', async () => {
-//         try {
-//             const result = await LocationDao.getHuts(invalidHuts[0]);
-//         } catch (err) {
-//             console.log("err " + err);
-//             return expect(err).toEqual(400);
-//         }
-
-//     });
-// });
