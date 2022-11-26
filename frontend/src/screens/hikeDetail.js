470cbf85667188adcb74f31eb1ba48c048b02633
Enrico Jansen <s304160@studenti.polito.it>
2022-11-26 15:53:59 +0100

    Fixed hikeDetail.js

--- frontend/src/screens/hikeDetail.js
+++ frontend/src/screens/hikeDetail.js
@@ -11,55 +11,6 @@ const HikeDetail = ({ props, setProps }) => {
     const navigate = useNavigate();
     const params = useParams();
 
-    const [openStart, setOpenStart] = useState(false);
-    const [openEnd, setOpenEnd] = useState(false);
-    const [startPoint, setStartPoint] = useState();
-    const [endPoint, setEndPoint] = useState();
-    const [locationList, setLocationList] = useState([]);
-    const [startPointHike, setStartPointHike] = useState(hike.startPt);
-    const [endPointHike, setEndPointHike] = useState(hike.endPt);
-    const [error, setError] = useState(false);
-
-    /*useEffect(() => {
-        API.getHutsAndParkingLots().then(res => {
-            setHikes([]);
-            res.forEach((hike, index) => {
-                setHikes(hikes => [...hikes, JSON.stringify(hike)]);
-            });
-            // console.log(hikes);
-            setIsLoading(false);
-        }).catch(error => console.log(error));
-    }, [props.filters])*/
-
-    // const handleStartSubmit = (event) => {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     if (!isNaN(parseInt(startPoint, 10))) {
-    //         API.setHikeStartPoint(hike.id, startPoint).then().catch(error => console.log(error));
-    //         setStartPointHike(locationList.find(location => location.id === parseInt(startPoint, 10)));
-    //         setOpenStart(false);
-    //         setError(false);
-    //     }
-    //     else {
-    //         setError(true);
-    //     }
-
-    // }
-
-    // const handleArrivalSubmit = (event) => {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     if (!isNaN(parseInt(endPoint, 10))) {
-    //         API.setHikeEndPoint(hike.id, endPoint).then().catch(error => console.log(error));
-    //         setEndPointHike(locationList.find(location => location.id === parseInt(endPoint, 10)));
-    //         setOpenEnd(false);
-    //         setError(false);
-    //     }
-    //     else {
-    //         setError(true);
-    //     }
-    // }
-
     return (
         <Container>
             <Row>
@@ -74,96 +25,6 @@ const HikeDetail = ({ props, setProps }) => {
                 </Col>
             </Row>
             <ul></ul>
-            {/* {(props.user !== undefined && props.user.role === "guide") ?
-                <div>
-                    <Row>
-                        <Col>
-
-                            <Button onClick={() => { setOpenStart(!openStart); setStartPoint(); setError(false); API.getHutsAndParkingLots().then(locations => { setLocationList(locations) }).catch(error => console.log(error)); }}
-                                aria-controls="example-collapse-text"
-                                aria-expanded={openStart} variant="success" size="lg"><h4 className="text-white"> Add Start Point</h4></Button>
-                        </Col>
-                        <Col>
-                            <Button onClick={() => { setOpenEnd(!openEnd); setEndPoint(); setError(false); API.getHutsAndParkingLots().then(locations => { setLocationList(locations) }).catch(error => console.log(error)); }}
-                                aria-controls="example-collapse-text"
-                                aria-expanded={openEnd} variant="success" size="lg"><h4 className="text-white"> Add End Point</h4></Button>
-                        </Col>
-                    </Row>
-                    <ul></ul>
-                    <Row>
-                        <Collapse in={openStart}>
-                            <Form onSubmit={handleStartSubmit}>
-                                <Form.Group>
-                                    <h5>Select Start Point: </h5>
-                                    <Form.Select value={startPoint}
-                                        onChange={e => setStartPoint(e.target.value)}
-                                        aria-label="region" size="lg">
-                                        <option value={undefined}>Select the Start Point</option>
-                                        {locationList.map((location, index) => <option value={location.id} key={index}>{location.name}</option>)}
-                                    </Form.Select>
-                                </Form.Group>
-                                <ul></ul>
-                                {error === true ?
-                                    <h5>Errore nella selezione</h5>
-                                    :
-                                    false
-                                }
-                                <ul></ul>
-                                <Row>
-                                    <Col md={10} xs={8}>
-                                        <Button variant="danger" onClick={() => { setStartPoint(); setOpenStart(false); setError(false); }} size="lg">
-                                            Back
-                                        </Button>
-                                    </Col>
-                                    <Col md={2} xs={4}>
-                                        <Button variant="success" type="submit" size="lg">
-                                            Confirm
-                                        </Button>
-                                    </Col>
-                                </Row>
-                                <ul></ul>
-                            </Form>
-                        </Collapse>
-
-                        <Collapse in={openEnd}>
-                            <Form onSubmit={handleArrivalSubmit}>
-                                <Form.Group>
-                                    <h5>Select Arrival Point: </h5>
-                                    <Form.Select value={endPoint}
-                                        onChange={e => setEndPoint(e.target.value)}
-                                        aria-label="region" size="lg">
-                                        <option value={undefined}>Select the Arrival Pont</option>
-                                        {locationList.map((location, index) => <option value={location.id} key={index}>{location.name}</option>)}
-                                    </Form.Select>
-                                </Form.Group>
-                                <ul></ul>
-                                {error === true ?
-                                    <h5>Errore nella selezione</h5>
-                                    :
-                                    false
-                                }
-                                <ul></ul>
-                                <Row>
-                                    <Col md={10} xs={8}>
-                                        <Button variant="danger" onClick={() => { }} size="lg">
-                                            Back
-                                        </Button>
-                                    </Col>
-                                    <Col md={2} xs={4}>
-                                        <Button variant="success" type="submit" size="lg">
-                                            Confirm
-                                        </Button>
-                                    </Col>
-                                </Row>
-                                <ul></ul>
-                            </Form>
-                        </Collapse>
-                    </Row>
-                    <ul></ul>
-                </div>
-                :
-                false
-            } */}
             <Row style={{ flex: 1, marginTop: "2%", alignItems: "center" }}>
                 <Col>
                     <h3>
@@ -190,7 +51,7 @@ const HikeDetail = ({ props, setProps }) => {
 
                 </Col>
                 <Col>
-                    <Map startPt={JSON.stringify(startPointHike)} endPt={JSON.stringify(endPointHike)} />
+                    <Map startPt={JSON.stringify(hike.startPt)} endPt={JSON.stringify(hike.endPt)} file={hike.track} />
                 </Col>
             </Row>
             <ul></ul>
